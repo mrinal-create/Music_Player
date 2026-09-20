a1ebca04fab2307f7d0770ae336095a933efeb59
Mrinal Upreti <upretimrinal@gmail.com>
2026-09-20 17:21:20 +0530

    feat: add terminal music player interface

--- app.js
+++ app.js
@@ -3,21 +3,42 @@ const path = require("path");
 
 const songsDir = path.join(__dirname, "songs");
 
+let index = 0;
+
 const getSongs = () => {
     return fs.readdirSync(songsDir)
         .filter(f => f.toLowerCase().endsWith(".mp3"));
 };
 
-const songs = getSongs();
+const showSongs = () => {
+    console.clear();
 
-console.log("🎵 TERMINAL PLAYER\n");
+    const songs = getSongs();
 
-if (!songs.length) {
-    console.log("No songs found.");
-} else {
-    console.log("Available Songs:\n");
+    console.log("=================================");
+    console.log("        🎵 TERMINAL PLAYER");
+    console.log("=================================\n");
 
-    songs.forEach((song, i) => {
-        console.log(`${i} : ${song}`);
-    });
-}
+    console.log("ALL SONGS\n");
+
+    if (!songs.length) {
+        console.log("No songs found.\n");
+    } else {
+        songs.forEach((song, i) => {
+            console.log(`${i === index ? ">" : " "} ${i} : ${song}`);
+        });
+    }
+
+    console.log("\n---------------------------------");
+    console.log("↑ ↓  : Select");
+    console.log("ENTER: Play");
+    console.log("A    : Pause");
+    console.log("D    : Resume");
+    console.log("1    : Favourites");
+    console.log("2    : All Songs");
+    console.log("Q    : Quit");
+
+    console.log("\n■ No song playing");
+};
+
+showSongs();
